997d4ed7eda7b7a727ef90d217b214105c5ae6c0
甄镭 <zhenlei1970@hotmail.com>
2022-01-24 10:40:26 +0800

    add pudata

---
+++ Plat.Blazor/Data/PuService.cs
@@ -0,0 +1,20 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Plat.Blazor.Data
+{
+    public class PuService
+    {
+        private readonly static  string connectionString= "server=192.168.0.4;database=platsql;uid=sa;pwd=82354699";
+        public static List<dynamic> QueryIn()
+        {
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                var sql = "select * from SUF_PROGRAMUNIT_TB";
+                //参数类型是Array的时候，dappper会自动将其转化
+                return connection.Query<dynamic>(sql).ToList();
+            }
+        }
+    }
+}
